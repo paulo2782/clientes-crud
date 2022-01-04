fe861d14bbed28073a6fc49658a4899dfeccc64c
Paulo Oliveira <paulo2782@gmail.com>
2022-01-04 17:59:40 -0300

    concluindo crud

--- public/js/crud.js
+++ public/js/crud.js
@@ -1,11 +1,15 @@
-
+// CLIENTES
+// Todos Clientes
 allClient = () => {
+    $('#data_client').empty()
     $.ajax({
         type: "get",
         url: "http://127.0.0.1:8000/api/allClient",
         success: function (response) {
-            response.forEach(i => {
-                $('#data_client').html(
+            console.log(response)
+             response.forEach(i => {
+                $('#data_client').append(
+                    '<tr>'+
                     '<td>'+i.name+'</td>'+
                     '<td>'+i.cpf_cnpj+'</td>'+
                     '<td>'+i.address+'</td>'+
@@ -23,30 +27,75 @@ allClient = () => {
                         "class='btn btn-danger form-control' "+
                         "title='Excluir Registro' "+
                         "onclick='deleteClient(id="+i.id+")'> "+
-                    "</td>"
+                    "</td>"+
+                    // Botão Modal Visualiza Dependetes 
+                    "<td>"+
+                    "<button type='button' class='btn btn-info' onclick='showDependents(id="+i.id+")' data-bs-toggle='modal' data-bs-target='#modelId'>"+
+                    "Visualiza dependentes"+
+                    "</button>"+
+                    "</tr>"
                 )
-                    
             });
         }
     });
 }
 
+// DIV CADASTRO DE CLIENTE
 newClient = () => {
     if($('.cardNewClient').is(':visible')){
-        $('.cardNewClient').fadeOut(1000)
-    }else{
-        $('.cardNewClient').fadeIn(1000)
-    }
+        $('.cardNewClient').fadeOut(500)
 
+    }else{
+        $('#titleCardClient').html('Cadastro de Cliente')
+        $('.cardNewClient').fadeIn(500)
+        $('.cardNewDependent').hide()
+
+    }
 }
 
-saveClient = () => {
-    if(confirm('Deseja salvar cliente?')) {
-        alert('Registro Salvo.')
-    }else{
+
+$('#btnSaveClient').click(function(){
+    nameClient = $('#nameClient').val()
+    cpf_cnpj   = $('#cpf_cnpj').val()
+    cep        = $('#cep').val()
+    address    = $('#address').val()
+
+    //Mostra DIV Mensagem
+    $('#message').show()
+    $('#message').fadeOut(5000)
+
+    // Validação campos do formulário
+    if(nameClient.length == 0){
+        $('#message').html("Nome cliente não pode ser em branco.")
         return false;
     }
-}
+    if(cpf_cnpj.length == 0){
+        $('#message').html("CPF ou CNPJ não pode ser em branco.")
+        return false;
+    }    
+    if(cep.length == 0){
+        $('#message').html("CEP não pode ser em branco.")
+        return false;
+    }    
+    if(address.length == 0){
+        $('#message').html("Endereço não pode ser em branco.")
+        return false;
+    }
+
+    if(confirm('Deseja salvar cliente?')){
+        $('#message').html('')
+        $.ajax({
+            type:"POST",
+            url: "http://127.0.0.1:8000/api/createClient",
+            data: $('#crud_client').serialize(),
+            success: function (response) {
+                $('#message').show()
+                $('#message').html(response.message)
+                allClient() 
+            }
+        })
+    }   
+})
 
 updateClient = (id) => {
     if(confirm('Deseja atualizar dados do cliente?')) {
@@ -55,11 +104,106 @@ updateClient = (id) => {
         return false;
     }
 }
+
+// Deletar Cliente
 deleteClient = (id) => {
     if(confirm('Confirma excluir cliente?')) {
-        alert('Registro excluído.')
+        $.ajax({
+            type: "DELETE",
+            url: "http://127.0.0.1:8000/api/deleteClient/"+id,
+            success: function (response) {
+                $('#message').show()
+                $('#message').html(response.message)
+                $('#message').fadeOut(5000)
+                allClient() 
+            }
+        })
     }else{
         return false;
     }
 
 }
+
+// DEPENDENTES
+// Novo Dependente
+
+newDependent = () => {
+    if($('.cardNewDependent').is(':visible')){
+        $('.cardNewDependent').fadeOut(500)
+    }else{
+        $('#titleCardDependent').html('Cadastro de Dependente')
+        $('.cardNewDependent').fadeIn(500)
+        $('.cardNewClient').hide()
+    }
+
+    // Carrega Select Option Cliente
+    $('#clients_id').empty()
+    $.ajax({
+        type: "get",
+        url: "http://127.0.0.1:8000/api/allClient",
+        success: function (response) {
+            response.forEach(i => {
+                $('#clients_id').append("<option value='"+i.id+"'>"+i.name+"</option>")
+            })
+        }    
+    })
+
+}
+
+showDependents = (id) => {
+    $('#listDependents').html('')
+    $.ajax({
+        type:"GET",
+        url: "http://127.0.0.1:8000/api/allDepentsClient/"+id,
+        success: function (response) {
+            
+            $('#listDependents').html('<h6>Dependentes do Cliente: '+response[0].name_client+'</h6>')
+            response.forEach(i => {
+                $('#listDependents').append("Nome: "+i.name+" - Idade: "+i.age+" anos<br>")
+            })
+            
+        }
+    })
+}
+
+$('#btnSaveDependent').click(function(e){
+    nameDependent = $('#nameDependent').val()
+    age           = $('#age').val()
+    date_birth    = $('#date_birth').val()
+ 
+    //Mostra DIV Mensagem
+    $('#message').show()
+    $('#message').fadeOut(5000)
+
+    // Validação campos do formulário
+    if(nameDependent.length == 0){
+        $('#message').html("Nome dependente não pode ser em branco.")
+        return false;
+    } 
+    if(age.length == 0){
+        $('#message').html("Idade não pode ser vazio.")
+        return false;
+    } 
+    if(date_birth.length == 0){
+        $('#message').html("Data nascimento não pode ser vazio.")
+        return false;
+    } 
+    
+
+    if(confirm('Deseja salvar dependente?')){
+        $('#message').html('')
+        $.ajax({
+            type:"POST",
+            url: "http://127.0.0.1:8000/api/createDependent",
+            data: $('#crud_dependent').serialize(),
+            success: function (response) {
+                $('#message').show()
+                $('#message').html(response.message)
+                allClient() 
+            }
+        })
+        
+    }
+})
+
+ 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="pt-br">
   <head>
-    <title>Title</title>
+    <title>Electra CRUD</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -9,27 +9,43 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  </head>
-  <body>
     <!-- Bootstrap JavaScript Libraries -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 
-    <!-- Requisições API -->
-    <script src="{{asset('js/crud.js')}}"></script>
 
-    <nav class="nav justify-content-center bg-default">
-      <a class="nav-link active" href="#">ELECTRA - CRUD</a>
-      <a class="nav-link" href="#">Clientes</a>
+  </head>
+  <body>
+  <form id="crud_client">
+    <nav class="nav justify-content-center bg-dark" style="color:white;padding:10px">
+            <label>ELECTRA - CRUD</crud>
     </nav>    
+    <div class="container" style="margin-top:80px">
+    <!-- CARD CADASTRO DO CLIENTE -->
+    <div class="row">
+        <div class="col-lg-12">
+            <!-- DIV DE MENSAGENS -->
+            <div 
+                class="alert alert-danger" 
+                role="alert" 
+                id="message" 
+                style="display:none;position:fixed;margin-top:-70px">
+            </div>
+        </div>
 
-    <div class="container">
-        <div class="row">
-        <div class="col-lg-12 cardNewClient" style="display:none">
+        <div class="col-lg-12 cardNewClient">
         <div class="card">
            <div class="card-body">
+                <h5 id="titleCardClient">Cadastro de Cliente</h5>
                 Nome:
-                <input type="text" id="name" class="form-control" placeholder="Nome do Cliente" autocomplete="off">
+                <input 
+                    type="text" 
+                    id="nameClient" 
+                    name="name"
+                    class="form-control" 
+                    placeholder="Nome do Cliente" 
+                    autocomplete="off"
+                />
                 <div class="form-check">
                     <input class="form-check-input" type="radio" name="radio" id="radio1" checked>
                     <label class="form-check-label">
@@ -44,49 +60,161 @@
                 </div>
 
                 <label id="lbl_cpf_cnpj"> CPF </label>
-                <input type="text" id="cpf_cnpj" class="form-control" autocomplete="off" maxlength="18">
+
+                <input 
+                    type="text" 
+                    name="cpf_cnpj" 
+                    id="cpf_cnpj" 
+                    class="form-control" 
+                    autocomplete="off" 
+                    maxlength="18"
+                />
                 CEP:
-                <input type="text" id="cep" class="form-control" placeholder="CEP" autocomplete="off">
+                <input 
+                    type="text" 
+                    name="cep" 
+                    id="cep" 
+                    class="form-control" 
+                    placeholder="CEP" 
+                    autocomplete="off"
+                />
                 Endereço:
-                <input type="text" id="address" class="form-control" placeholder="Endereço do Cliente" autocomplete="off">
+                <input 
+                    type="text" 
+                    name="address" 
+                    id="address" 
+                    class="form-control" 
+                    placeholder="Endereço do Cliente" 
+                    autocomplete="off"
+                />
             </div>
-            <input type="button" value="Salvar" class="btn btn-primary" onclick="saveClient()">
+            <input type="button" value="Salvar" class="btn btn-primary" id="btnSaveClient">
+        </div>
+        </div>
+    </div>
+  </form>
+
+  <form id="crud_dependent" >
+    <!-- CARD DEPENDENTES DO CLIENTE -->
+    <div class="row">
+        <div class="col-lg-12 cardNewDependent" style="display:none">
+        <div class="card">
+           <div class="card-body">
+                <h5 id="titleCardDependent"></h5>
+                <label class="form-label">Nome cliente:</label>
+                <select class="form-control" name="clients_id" id="clients_id">
+                </select>
+
+                Nome:
+                <input 
+                    type="text" 
+                    id="nameDependent" 
+                    name="name"
+                    class="form-control" 
+                    placeholder="Nome do Dependente" 
+                    autocomplete="off"
+                />
+
+                <label id="lbl_cpf_cnpj"> Idade: </label>
+
+                <input 
+                    type="number" 
+                    name="age" 
+                    id="age" 
+                    class="form-control" 
+                    min="0"
+                    maxlength="3"
+                />
+                Data nascimento:
+                <input 
+                    type="date" 
+                    name="date_birth" 
+                    id="date_birth" 
+                    class="form-control" 
+                />
+            </div>
+            <input type="button" value="Salvar" class="btn btn-primary" id="btnSaveDependent">
         </div>
 
         </div>
-        </div>
-        <br>
-        <input type="button" value="Novo Cliente" class="btn btn-primary" onclick="newClient()">
-        <table class="table table-bordered">
-            <tr>
-                <th>NOME</th>
-                <th>CPF_CNPJ</th>
-                <th>ENDEREÇO</th>
-                <th>CEP</th>
-                <th></th>
-                <th></th>
-            </tr>
-        <tbody id="data_client">
-        
-        </tbody>
+    </div>
+    <br>
+
+    <input type="button" value="Novo Cliente" class="btn btn-primary" onclick="newClient()">
+    <input type="button" value="Dependentes"  class="btn btn-warning" onclick="newDependent()">
+
+    <table class="table table-bordered">
+        <tr>
+            <th>NOME</th>
+            <th>CPF / CNPJ</th>
+            <th>ENDEREÇO</th>
+            <th>CEP</th>
+            
+        </tr>
+    <tbody id="data_client">
+    </tbody>
     </table>
+
+    <!-- MODAL VISUALIZAR DEPENDENTES -->
+
+    <div class="modal fade" id="modelId" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                    <div class="modal-header">
+                            <h6 class="modal-title" id="titleModal"></h6>
+
+                        </div>
+                <div class="modal-body">
+                    <div class="container-fluid" id="listDependents">
+                        
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+
+
+    <script>
+        var modelId = document.getElementById('modelId');
+    
+        modelId.addEventListener('show.bs.modal', function (event) {
+              // Button that triggered the modal
+              let button = event.relatedTarget;
+              // Extract info from data-bs-* attributes
+              let recipient = button.getAttribute('data-bs-whatever');
+    
+            // Use above variables to manipulate the DOM
+        });
+    </script>
+    
   </body>
 </html>
+</form>
 
 <script src="{{asset('js/jquery.mask.min.js')}}"></script>
+<!-- Requisições API -->
+<script src="{{asset('js/crud.js')}}"></script>
+
 <script>
-allClient();
-$('#cpf_cnpj').mask('999.999.999-99')
-$('#radio1').change(function(e){
-    $('#lbl_cpf_cnpj').html('CPF')
-    $('#cpf_cnpj').val('')
+$(function() {
+    allClient();
     $('#cpf_cnpj').mask('999.999.999-99')
-})
-$('#radio2').change(function(e){
-    $('#lbl_cpf_cnpj').html('CNPJ')
-    $('#cpf_cnpj').val('')
-    $('#cpf_cnpj').mask('99.999.999/9999-99')
-})
+    $('#cep').mask('99.999-999')
+    $('#radio1').change(function(e){
+        $('#lbl_cpf_cnpj').html('CPF')
+        $('#cpf_cnpj').val('')
+        $('#cpf_cnpj').mask('999.999.999-99')
+    })
+    $('#radio2').change(function(e){
+        $('#lbl_cpf_cnpj').html('CNPJ')
+        $('#cpf_cnpj').val('')
+        $('#cpf_cnpj').mask('99.999.999/9999-99')
+    })
+})    
 </script>
 
 
