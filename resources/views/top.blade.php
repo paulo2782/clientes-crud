bb9e03370a6c52e0be0d6e8b288666b76362e6d6
paulo2782 <paulo2782@gmail.com>
2022-01-26 15:45:26 -0300

    Update top.blade.php

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -1,6 +1,6 @@
 <nav 
     class="nav justify-content-center" 
     style="background-color:#527A4B;color:white;padding:5px;position:fixed;z-index:1;top:0;width:100%">
-    <label style="font-size:20px;font-family:Helvetica">ELECTRA - CRUD</crud>
+    <label style="font-size:20px;font-family:Helvetica">CLIENTES E DEPENDENTES - CRUD</crud>
 </nav>    
 
